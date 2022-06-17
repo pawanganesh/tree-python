fa838986ea3d05144aaf02191e549427ec21ec30
pawanlalganesh <pawanlalganesh@gmail.com>
2022-06-17 15:51:09 +0545

    used generator

--- tree.py
+++ tree.py
@@ -5,26 +5,19 @@ with open('input.json') as f:
     data = json.load(f)
 
 
-# def has_branch(node):
-#     try:
-#         node['branches']
-#         return True;
-#     except:
-#         return False;
-
-
-def get_leaves(node):
+def solution(data):
     leaves = []
+    req = []
+
+    node = {"branches": data}
+
     if 'branches' in node:
         for branch in node['branches']:
             leaves.extend(get_leaves(branch))
     else:
         leaves.extend(node['leaves'])
-    return leaves;
+    yield leaves
 
-
-def get_pair(data):
-    req = []
     if(type(data) == dict):
         # current data is a node
         id = data['id']
@@ -36,10 +29,10 @@ def get_pair(data):
         for branch in data:
             req.extend(get_pair(branch))
 
-    return req    
+    yield req
 
-print(get_leaves({
-    "branches": data,
-}))
 
-print(get_pair(data))
+sol = solution(data)
+
+print(next(sol))
+print(next(sol))
